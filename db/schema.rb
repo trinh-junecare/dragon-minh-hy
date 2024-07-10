d537b362217f5132222a27302b752d5154a3b59f
Trinh Nguyen <trinh@junecare.co>
2024-07-10 17:07:37 -0400

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_09_154751) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_10_210520) do
+  create_table "visitors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "vitals", force: :cascade do |t|
     t.string "name"
     t.integer "weight"
